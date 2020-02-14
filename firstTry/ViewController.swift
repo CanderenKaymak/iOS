028648915e391343caa6906589fa26ef5eb644c2
Canderen Kaymak <canderenkaymak@gmail.com>
2020-02-14 18:21:09 +0300

    Initial Commit

---
+++ firstTry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firstTry
+//
+//  Created by Canderen Kaymak on 14.02.2020.
+//  Copyright © 2020 Canderen Kaymak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
